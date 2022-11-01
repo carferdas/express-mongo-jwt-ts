47bced93b81f81bb893e23ab2ec2f2b6e459d0ec
Carlos Fernandes <carferdas@gmail.com>
2022-11-01 10:20:17 -0300

    remove build

--- build/routes/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Routes = void 0;
-const express_1 = require("express");
-const auth_route_1 = require("./auth.route");
-exports.Routes = (0, express_1.Router)();
-exports.Routes.use('/', auth_route_1.AuthRoutes);
